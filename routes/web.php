0e991a3fe8f08814f3e350d20194ed5051405464
abrahamat363-blip <abrahamat363@gmail.com>
2025-11-30 16:02:37 -0700

    proyecto laravel inicial

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\OrderController;
+
+Route::resource('products', ProductController::class);
+Route::resource('customers', CustomerController::class);
+Route::resource('orders', OrderController::class);
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
